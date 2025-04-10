fa7d356ef392794c60c464cccde21c55925a5fa0
Morgan Powers <morganpowers@jessicas-air-3.trinity.local>
2025-04-09 22:00:06 -0500

    jumps - NEEDS FIXING

--- calculator.asm
+++ calculator.asm
@@ -6,14 +6,13 @@
     subtract:  .ascii "-"
     multiply:  .ascii "*"
     divide:    .ascii "/"
-    quit:      .ascii "q"
+
 
 .text
     la $t3,addition
     la $t4,subtract
     la $t5,multiply
     la $t6,divide
-    la $t7,quit
 
     IO:
 #load first num into $t0
@@ -24,45 +23,57 @@
         syscall
         move $t0,$v0
         
-#load op into $t1
-        la $a0,promptOp
-        li $v0,4
-        syscall
-        li $v0,8
-        syscall
-        move $t1,$v0
-#if user inputs q, exit. Not working.
-	beq $t1,$t5,exit
-
-#load second num into $t2       
+#load second num into $t1       
         la $a0,promptNum
         li $v0,4
         syscall
         li $v0,5
         syscall
+        move $t1,$v0
+        
+#load op into $t2
+        la $a0,promptOp
+        li $v0,4
+        syscall
+        li $v0,8
+        syscall
         move $t2,$v0
+
+    jump:
+#Not working.
+	beq $t2,$t3,addit
+	beq $t2,$t4,subit
+	beq $t2,$t5,multit
+	beq $t2,$t6,divit
+	j exit
+
+
                 
-    add:
-        add $a0,$t0,$t2
+    addit:
+        add $a0,$t0,$t1
         li $v0,1
         syscall
+        j IO
         
-    sub:
-        sub $a0,$t0,$t2
+    subit:
+        sub $a0,$t0,$t1
         li $v0,1
         syscall
+        j IO
         
-    mult:
-        mult $t0,$t2
+    multit:
+        mult $t0,$t1
         mflo $a0 #mult is weird
         li $v0,1
         syscall
+        j IO
         
-    div:
-        div $t0,$t2
+    divit:
+        div $t0,$t1
         mflo $a0
         li $v0,1
         syscall
+        j IO
 
     exit:
         li $v0,10
