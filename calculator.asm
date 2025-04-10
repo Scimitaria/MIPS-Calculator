bfac04ba611c147b3bf5b76fac1acf3c0b051c98
Owen Story <ostory674@gmail.com>
2025-04-10 13:48:32 -0500

    added an error check and exponents, also refactored to use ascii values for character checks instead of constants.

--- calculator.asm
+++ calculator.asm
@@ -1,98 +1,125 @@
 .data
-    promptFst:	.asciiz "\nEnter the first integer: "		# requires a \n because the previous print is the result which has no implicit \n
+    promptFst:	.asciiz "\nEnter the first integer: "		# requires a \n because the previous print is a char which has no implicit \n
     promptSnd:	.asciiz "Enter the second integer: "		# does not require a \n because the previous print is an int which has implicit \n
-    promptOp: 	.asciiz "Enter an operation (+,-,*,/): "	# same here
+    promptOp: 	.asciiz "Enter an operation (+,-,*,/,^): "	# same here
     result:    	.asciiz "\nResult: "				# requires a \n because the previous print is a char which does not have an implicit \n
+    continue:	.asciiz "\nContinue? (y/n): "			# requires a \n because the previous print is the result which does not have an implicit \n
+    notOp:	.asciiz "\nInvalid operator, please try again."	# requires a \n because the previous print is a char which does not have an implicit \n
     equals:	.asciiz " = "
-    
-    addition:  	.byte '+'
-    subtract:  	.byte '-'
-    multiply:  	.byte '*'
-    divide:    	.byte '/'
 
-#load the operation bytes into a s1-s4
 .text
-    la $t1,addition
-    lb $s1,0($t1)
-    la $t2,subtract
-    lb $s2,0($t2)
-    la $t3,multiply
-    lb $s3,0($t3)
-    la $t4,divide
-    lb $s4,0($t4)
-
+    # handle the IO logic
     IO:
-#load first num into $t0
+	# load first integer into $s0
         la $a0,promptFst
         li $v0,4
         syscall
         li $v0,5
         syscall
-        move $t0,$v0
+        move $s0,$v0
         
-#load second num into $t1       
+	# load second integer into $s1       
         la $a0,promptSnd
         li $v0,4
         syscall
         li $v0,5
         syscall
-        move $t1,$v0
+        move $s1,$v0
         
-#load op into $t2
+	#load operation character into $s2
         la $a0,promptOp
         li $v0,4
         syscall
         li $v0,12
         syscall
-        move $t2,$v0
+        move $s2,$v0
 
+    # handle the operator jumping logic
     jump:
-    	beq $t2,$s1,addit
-    	beq $t2,$s2,subit
-    	beq $t2,$s3,multit
-    	beq $t2,$s4,divit
-	j exit
+    	li $t0,43		# ascii value for +
+    	beq $s2,$t0,addit
+    	li $t0,45		# ascii value for -
+    	beq $s2,$t0,subit
+    	li $t0,42		# ascii value for *
+    	beq $s2,$t0,multit
+    	li $t0,47		# ascii value for /
+    	beq $s2,$t0,divit
+    	li $t0,94		# ascii value for ^
+    	beq $s2,$t0,expit
+	j retry
                 
+    # if the operator is not an option, print an error and prompt for input again
+    retry:
+    	la $a0,notOp
+    	li $v0,4
+    	syscall
+    	j IO
+        
     addit:
-        add $t3,$t0,$t1
+        add $s3,$s0,$s1
         j print
         
     subit:
-        sub $t3,$t0,$t1
+        sub $s3,$s0,$s1
         j print
         
     multit:
-        mult $t0,$t1
-        mflo $t3 #mult is weird
+        mult $s0,$s1
+        mflo $s3
         j print
         
     divit:
-        div $t0,$t1
-        mflo $t3
+        div $s0,$s1
+        mflo $s3
         j print
+    
+    # does not work with really large outputs, max output is still only 32 bits
+    expit:
+    	li $t0,1
+    	move $s3,$s0
+    	loop:
+    	    bge $t0,$s1,print
+    	    mult $s3,$s0
+            mflo $s3
+            addi $t0,$t0,1
+            j loop
         
-    #formats the print statement all fancy, assumes the output is stored in t3
+    # formats the print statement all fancy, assumes the output is stored in s3
     print:
      	la $a0,result
     	li $v0,4
     	syscall
-    	move $a0,$t0
+    	move $a0,$s0
      	li $v0,1
     	syscall
-     	move $a0,$t2
+     	move $a0,$s2
      	li $v0,11
     	syscall
-    	move $a0,$t1
+    	move $a0,$s1
      	li $v0,1
     	syscall
     	la $a0,equals
      	li $v0,4
     	syscall   	
-    	move $a0,$t3
+    	move $a0,$s3
     	li $v0,1
     	syscall
-    	j IO
-
+	j check
+    	
+    # check whether the user wants to continue the program
+    check:
+    	la $a0,continue
+        li $v0,4
+        syscall
+        li $v0,12
+        syscall
+        li $t0,121		# ascii value for y
+        beq $v0,$t0,IO
+        li $t0,89		# ascii value for Y
+        beq $v0,$t0,IO		
+        j exit
+       
+    # exit the program cleanly
     exit:
         li $v0,10
         syscall
